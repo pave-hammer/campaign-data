0be6e3a1fa79311c7fcd3168e6036bcd0d9fcd61
Mike <szekleym@gmail.com>
2019-01-18 12:41:25 -0700

    added specified get route for singling data out

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
 const express = require("express")
 const app = express()
+const port = 8000
+const data = require("./data.json")
+const cors = require("cors")
+
+app.use(cors())
+
+app.get("/", (req, res, next) => {
+  res.status(200).send({
+    "message": "You have the data.",
+    "data": data
+  })
+})
+
+app.get("/:faction", (req, res, next) => {
+  const selectedFaction = req.params.faction
+  if(!data.factions.includes(selectedFaction)){
+    res.status(404).send("Requested resource was not found.")
+  } else {
+    const matching = data.catagory.filter(catagory => catagory.faction.includes(selectedFaction))
+    res.status(200).send(matching)
+  }
+})
+
+app.use((req, res, next) => {
+  res.status(404).send("Requested resource was not found.")
+})
+
+app.listen(port, () => console.log(`Your data is available at http://localhost:${port}`))
