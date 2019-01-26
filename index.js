a2eb4aa70ee6700af14c23d847eeb75f7fe66d62
Mike <szekleym@gmail.com>
2019-01-26 14:36:39 -0700

    testing routes and joins

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const port = 8000
 const environment = process.env.NODE_ENV || "development";
 const knexconfig = require("./knexfile.js")[environment];
 const db = require("knex")(knexconfig);
+const bodyParser = require("body-parser");
 
-app.get("/codex", (req, res, next) => {
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.get("/", (req, res, next) => {
   db.select("*").from("organizations")
   .then((rows) => {
     res.send(rows)
@@ -15,14 +21,9 @@ app.get("/codex", (req, res, next) => {
   })
 })
 
-app.get("/codex/carinhall", (req, res, next) => {
-  db.select("*").from("organizations")
-  .then((rows) => {
-    res.send(rows)
-  })
-  .catch((err) => {
-    next(err)
-  })
-})
+app.post("/codex"), (req, res, next) => {
+  console.log("It works")
+  res.send("It works")
+}
 
 app.listen(port, () => console.log(`Porty on port ${port}!`))
