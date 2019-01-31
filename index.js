6e86cb9eebf1610709638a372f595fdcb82a4efd
Mike <szekleym@gmail.com>
2019-01-31 08:36:32 -0700

    will be changing data table formats soon - finishing CRUD functions

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express")
 const app = express()
 const port = 3005
+const cors = require("cors")
 const environment = process.env.NODE_ENV || "development";
 const knexconfig = require("./knexfile.js")[environment];
 const db = require("knex")(knexconfig);
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(cors())
 
 app.get("/", (req, res, next) => {
-  db.select("*").from("data")
+  db.select("*").from("organizations")
   .then(function(row) {
     res.send(row)
   })
@@ -18,23 +23,18 @@ app.get("/", (req, res, next) => {
   });
 })
 
-app.get("/:id", (req, res, next) => {
-  db.schema.createTable("data_ranks", table => {
-    table.increments()
-    table.string('name').references('name').inTable("data").notNullable().defaultsTo('')
-    table.text('description').references('description').inTable('data').notNullable().defaultsTo('')
-  })
-})
-
-app.post("/data"), (req, res, next) => {
-  console.log(req.body)
-  knex('data').insert(req.body)
+app.post("/", (req, res, next) => {
+  db('organizations').insert(req.body)
   .then((rows) => {
     res.send(rows);
   })
   .catch((err) => {
     next(err);
   });
-}
+});
+
+app.put("/", (req, res, next) => {
+  console.log(req.body)
+})
 
 app.listen(port, () => console.log(`Porty on port ${port}!`))
