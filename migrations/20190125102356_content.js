a11ef0f369fefdee416ca7828297d86c538b074f
Mike <szekleym@gmail.com>
2019-02-15 15:06:48 -0700

    adding more data to seeds for testing purposes

--- migrations/20190125102356_content.js
+++ migrations/20190125102356_content.js
@@ -4,6 +4,10 @@ exports.up = function (knex, Promise) {
       table.string('name').notNullable().defaultsTo('')
       table.text('description').notNullable().defaultsTo('')
       table.integer('catagory_id').notNullable().defaultsTo(0)
+        .references('id')
+        .inTable('catagories')
+        .onDelete('CASCADE')
+        .index();
   })
 }
 
